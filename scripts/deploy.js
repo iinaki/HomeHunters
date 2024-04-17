3413ac266f469c6e2f40cf238aacae654fed5b71
iinaki <llorensinaki22@gmail.com>
2024-04-17 13:29:03 -0300

    contract y frontend

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,8 +6,48 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
+async function main() {
+  let [buyer, seller, inspector, lender] = ethers.getSigners()
+
+  const RealEstate = await ethers.getContractFactory('RealEstate')
+  realEstate = await RealEstate.deploy()
+  await realEstate.deployed()
+  console.log("RealEstate deployed to:", realEstate.address)
+  console.log("minting 3 properties...")
+
+  for(let i = 0; i < 3; i++){
+    let transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQVcpsjrA6cr1iJjZAodYwmPekYgbnXGo4DFubJiLc2EB/${i + 1}.json`)
+    await transaction.wait()
+  }
+
+  const Escrow = await ethers.getContractFactory('Escrow')
+  escrow = await Escrow.deploy(
+    realEstate.address,
+    seller.address,
+    inspector.address,
+    lender.address
+  )
+  await escrow.deployed()
+
+  for(let i = 0; i < 3; i++){
+    let transaction = await realEstate.connect(seller).approve(escrow.address, i + 1)
+    await transaction.wait()
+  }
+
+  transaction = await escrow.connect(seller).list(1,tokens(20),tokens(10), buyer.address)
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(2,tokens(15),tokens(5), buyer.address)
+  await transaction.wait()
+
+  transaction = await escrow.connect(seller).list(3,tokens(10),tokens(5), buyer.address)
+  await transaction.wait()
+
+  console.log("FINISHED")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
